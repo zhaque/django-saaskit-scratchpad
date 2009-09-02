30fdfcce414b0332f3134723f1bd04a0f6e2ad4b
CrowdSense <admin@crowdsense.com>
2009-09-02 20:06:26 +0100

    additional tags to support ui

--- scratchpad/templatetags/scratchpad_tags.py
+++ scratchpad/templatetags/scratchpad_tags.py
@@ -3,6 +3,7 @@ from django.template import Variable, resolve_variable
 from django.utils.translation import ugettext as _
 from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
+from todo.models import List, Item
 #from books.models import Book
 
 register = Library()
@@ -15,7 +16,21 @@ def get_add_to_html(html_id):
 
     return code
 
+def topitems(value, args):
+    arglist = args.split(',')
+    top = int(arglist[0])
+    userid = arglist[1]
+    useritems = value.item_set.get(assigned_to = userid)
+    if len(useritems) < 3:
+        for item in value.item_set:
+            if len(useritems) == 3:
+                break
+            else:
+                if item.assigned_to != userid:
+                    useritems.item_set.append(item)
+    return useritems
 
+register.filter('topitems', topitems)
 
 register.simple_tag(get_add_to_html)
 
--- scratchpad/views.py
+++ scratchpad/views.py
@@ -11,8 +11,10 @@ from django.template import RequestContext
 from muaccounts  import models as muamodels
 from scratchpad import models
 from scratchpad.forms import AddScratchPadForm, AddToScratchPad
+from todo.forms import AddListForm, AddItemForm, EditItemForm
 from todo import models as todomodels
 from threadedcomments import models as tmodels
+import datetime
 
 @login_required
 def view_list(request):
@@ -33,7 +35,7 @@ def view_list(request):
 @login_required
 def new_scratchpad(request):
     """ Shows new scratchpad form and does the actual form processing """
-
+    
     if request.POST:
         form = AddScratchPadForm(request.POST)
         item = form.save(commit=False)
@@ -47,8 +49,8 @@ def new_scratchpad(request):
         newtodo.save()
         item.tasks_list = newtodo
         item.save()
-        return HttpResponseRedirect(reverse('scratchpad-list'))
-
+        #return HttpResponseRedirect(reverse('scratchpad-list'))
+        return HttpResponseRedirect(reverse('scratchpad-scratchpad_view',args=[item.id]))
 
 
     form = AddScratchPadForm()
@@ -58,39 +60,154 @@ def new_scratchpad(request):
 def del_item(request, item_id):
     """Delete an item from a scratchpad """
 
-    if request.POST:
-        item = get_object_or_404(models.Item,id=item_id)
-        pad_id = item.scratchpad.id
-        item.delete()
+    #if request.POST:
+    item = get_object_or_404(models.Item,id=item_id)
+    pad_id = item.scratchpad.id
+    item.delete()
 
-        return HttpResponseRedirect(reverse('scratchpad-list'))
+    return HttpResponseRedirect(reverse('scratchpad-scratchpad_view',args=[pad_id]))
 
-    else:
-        item = get_object_or_404(models.Item,id=item_id)
+    #else:
+        #item = get_object_or_404(models.Item,id=item_id)
+        #task_list = todomodels.Item.objects.filter(list=item.scratchpad.tasks_list.id, list__account=request.muaccount, completed=0)
+        # Get all scratchpads connected via muaccounts with this user
 
-        return render_to_response("scratchpad/confirmdel_scratchpad_item.html", locals(), context_instance=RequestContext(request))
+        # get account scratchpads only if we have a muaccount!
+        #if getattr(request,"muaccount",False):
+
+        #    pads = models.ScratchPad.objects.filter(account=request.muaccount)
+        #else:
+        #    pads = None
+        #form = AddScratchPadForm()
+        
+        #return render_to_response("scratchpad/confirmdel_scratchpad_item.html", locals(), context_instance=RequestContext(request))
 
 @login_required
 def scratchpad_del(request, scratch_id):
     """ Delete a scratchpad """
 
-    if request.POST:
-        pad = get_object_or_404(models.ScratchPad,id=scratch_id)
-        pad.delete()
+    #if request.POST:
+    pad = get_object_or_404(models.ScratchPad,id=scratch_id)
+    pad.delete()
 
-        return HttpResponseRedirect(reverse('scratchpad-list'))
+    return HttpResponseRedirect(reverse('scratchpad-list'))
 
-    else:
-        pad = get_object_or_404(models.ScratchPad,id=scratch_id)
+    #else:
+        #pad = get_object_or_404(models.ScratchPad,id=scratch_id)
+        #task_list = todomodels.Item.objects.filter(list=pad.tasks_list.id, list__account=request.muaccount, completed=0)
+        # Get all scratchpads connected via muaccounts with this user
 
-        return render_to_response("scratchpad/confirmdel_scratchpad.html", locals())
+        # get account scratchpads only if we have a muaccount!
+        #if getattr(request,"muaccount",False):
+
+            #pads = models.ScratchPad.objects.filter(account=request.muaccount)
+        #else:
+            #pads = None
+        #form = AddScratchPadForm()
+        #return render_to_response("scratchpad/confirmdel_scratchpad.html", locals(), context_instance=RequestContext(request))
 
 @login_required
 def scratchpad(request, scratch_id):
     """ Shows list items in a scratchpad """
     pad = get_object_or_404(models.ScratchPad,id=scratch_id)
+    list = get_object_or_404(todomodels.List, slug=pad.tasks_list.slug)
+    listid = list.id
 
-    return render_to_response("scratchpad/view_scratchpad.html", locals())
+    # Get all scratchpads connected via muaccounts with this user
+
+    # get account scratchpads only if we have a muaccount!
+    if getattr(request,"muaccount",False):
+
+        pads = models.ScratchPad.objects.filter(account=request.muaccount)
+    else:
+        pads = None
+    form = AddScratchPadForm()
+    
+    thedate = datetime.datetime.now()
+    created_date = "%s-%s-%s" % (thedate.year, thedate.month, thedate.day)
+    
+    task_list = todomodels.Item.objects.filter(list=list.id, list__account=request.muaccount, completed=0)
+
+    if request.POST.getlist('add_task') :
+        form1 = AddItemForm(list, request.muaccount, request.POST,initial={
+        'assigned_to':request.user.id,
+        'priority':999,
+        })
+
+        if form1.is_valid():
+            # Save task first so we have a db object to play with
+            new_task = form1.save()
+
+            # Send email alert only if the Notify checkbox is checked AND the assignee is not the same as the submittor
+            # Email subect and body format are handled by templates
+            if "notify" in request.POST :
+                if new_task.assigned_to != request.user :
+
+                    # Send email
+                    email_subject = render_to_string("todo/email/assigned_subject.txt", { 'task': new_task })
+                    email_body = render_to_string("todo/email/assigned_body.txt", { 'task': new_task, 'site': current_site, })
+                    try:
+                        send_mail(email_subject, email_body, new_task.created_by.email, [new_task.assigned_to.email], fail_silently=False)
+                    except:
+                        request.user.message_set.create(message="Task saved but mail not sent. Contact your administrator." )
+
+            request.user.message_set.create(message="New task \"%s\" has been added." % new_task.title )
+            return HttpResponseRedirect(request.path)
+        else:
+            print form1.errors
+            #for error in form1.errors:
+             #   request.user.message_set.create(message=error[1])           
+    else:      
+        form1 = AddItemForm(list, request.muaccount, initial={
+            'assigned_to':request.user.id,
+            'priority':999,
+            } )
+
+    if request.POST:
+        if request.POST['scratchpad_type']:
+            item = models.Item()
+            item.notes = request.POST['content']
+            item.title = request.POST['title']
+
+            if request.POST['scratchpad_type'] == 'new':
+                spad = models.ScratchPad()
+                spad.title = request.POST['new_scratchpad']
+                spad.author = request.user
+                spad.account = request.muaccount
+
+                newtodo = todomodels.List()
+                newtodo.name = spad.title
+                newtodo.slug = "Tasks for scratchpad %s" % spad.title
+                newtodo.account = request.muaccount
+                newtodo.save()
+
+                spad.tasks_list = newtodo
+                spad.save()
+                item.scratchpad = spad
+                request.user.message_set.create(message="New Note added to '%s'" % spad.title)
+            else:
+                item.scratchpad = models.ScratchPad.objects.get(id=request.POST['scratchpad'])
+                request.user.message_set.create(message="New Note added to '%s'" % item.scratchpad.title)
+
+            item.save()
+
+
+            strcomment = request.POST['comment']
+            if strcomment != "":
+                comment = tmodels.ThreadedComment()
+                comment.content_object = item
+                comment.user = request.user
+                comment.comment = strcomment
+                comment.save()
+    
+    form2 = AddToScratchPad(None, request.muaccount)
+    comment = forms.CharField(widget=forms.Textarea).widget.render("comment","")
+
+    return render_to_response("scratchpad/view_scratchpad.html", locals(), context_instance=RequestContext(request))
+
+@login_required
+def scratchpad_items(request, scratch_id):
+    return HttpResponseRedirect(reverse('scratchpad-list-index'))
 
 @login_required
 def add_to(request):
@@ -101,6 +218,17 @@ def add_to(request):
 
         form = AddToScratchPad(data, request.muaccount)
         comment = forms.CharField(widget=forms.Textarea).widget.render("comment","")
+
+        # Get all scratchpads connected via muaccounts with this user
+
+        # get account scratchpads only if we have a muaccount!
+        if getattr(request,"muaccount",False):
+
+            pads = models.ScratchPad.objects.filter(account=request.muaccount)
+        else:
+            pads = None
+
+        scrapform = AddScratchPadForm()
 
         return render_to_response("scratchpad/addto_scratchpad.html", locals())
     else:
@@ -162,7 +290,19 @@ def save(request):
 def item(request, item_id):
     """ Shows scratchpad item details """
     item = get_object_or_404(models.Item,id=item_id)
+    list = get_object_or_404(todomodels.List, slug=item.scratchpad.tasks_list.slug)
 
+    # Get all scratchpads connected via muaccounts with this user
+
+    # get account scratchpads only if we have a muaccount!
+    if getattr(request,"muaccount",False):
+
+        pads = models.ScratchPad.objects.filter(account=request.muaccount)
+    else:
+        pads = None
+
+    scrapform = AddScratchPadForm()
+    task_list = todomodels.Item.objects.filter(list=list.id, list__account=request.muaccount, completed=0)
 
     return render_to_response("scratchpad/view_scratchpad_item.html", locals(), context_instance=RequestContext(request))
 
